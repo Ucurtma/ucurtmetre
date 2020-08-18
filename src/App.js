2780179ad15a163df90bb8a0ffd3705a22e607bb
afarukcali <afarukcali@gmail.com>
2020-08-19 01:01:25 +0300

    app router added and react router deps. Home and * path added.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import AppRouter from "./AppRouter/AppRouter";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <AppRouter />
     </div>
   );
 }
