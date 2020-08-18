2d53a172d4aed201a5ae9099eb90c5d5b433915f
afarukcali <afarukcali@gmail.com>
2020-08-19 01:01:41 +0300

    Some default components.

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  document.title = "Home";
+
+  return (
+    <div className="div-center">
+      <h4>Welcome to home page, Hello World</h4>
+    </div>
+  );
+};
+
+export default Home;
