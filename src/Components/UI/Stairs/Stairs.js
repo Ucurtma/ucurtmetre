d717c932ac7d8f33ab35d82028099bb1dfd9253c
Mustafa Turhan <mustaphaturhan@gmail.com>
2020-08-21 16:48:20 +0300

    review components, update responsive properties of thermometer and change structure of tabs

--- src/Components/UI/Stairs/Stairs.js
+++ src/Components/UI/Stairs/Stairs.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import './Stairs.scss';
-import useBreakpoints from '../../../Utils/useBreakpoints';
 
 const Stairs = () => {
-  const breakpoint = useBreakpoints();
-  const isMobile = breakpoint === 'isMobile';
-
-  return (
-    <div>
-      {!isMobile && (
-        <>
-          <div className="stair-bg" />
-          <div className="stair-front-bg" />
-        </>
-      )}
-      {isMobile && <div className="shaped-box" />}
-    </div>
-  );
+  return <div className="shaped-box" />;
 };
 
 export default Stairs;
--- src/Components/UI/Thermometer/Thermometer.js
+++ src/Components/UI/Thermometer/Thermometer.js
@@ -3,25 +3,42 @@ import './Thermometer.scss';
 import { Link } from 'react-router-dom';
 
 const Thermometer = () => {
+  const target = 240000;
+  const currentValue = 185210;
+  const thermometerHeight = 342;
+  const currentHeight = (currentValue * 100) / target;
+
   return (
     <div className="widget-thermometer">
       <div className="target-value">
         <span className="title">Hedeflenen Miktar</span>
         <span className="value">
-          240.000<span className="currency">TRYB</span>
+          {new Intl.NumberFormat('tr-TR').format(target)}
+          <span className="currency">TRYB</span>
         </span>
       </div>
-      <div className="thermometer">
-        <div className="current-value half">
+      <div className="thermometer-box" style={{ height: thermometerHeight }}>
+        {/* todo: add ruler
+          <div className="left">left</div>
+        */}
+        <div
+          className="current-value half"
+          style={{ height: `${currentHeight}%` }}
+        />
+        <div className="bottom" />
+        <div className="body" />
+        <div
+          className="right"
+          style={{ top: `calc(100% - ${currentHeight}%)` }}
+        >
           <div className="value">
             <div className="price">
-              185.210 <span className="currency">TRYB</span>
+              {new Intl.NumberFormat('tr-TR').format(currentValue)}
+              <span className="currency">TRYB</span>
             </div>
             <div className="subtext">Şimdiye Kadar Toplanan</div>
           </div>
         </div>
-        <div className="bottom" />
-        <div className="body" />
       </div>
       <Link className="button primary-button yellow">
         Tüm Kampanyalara Destek Ol
