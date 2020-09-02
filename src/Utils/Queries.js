19215c1b6108907508746fff0a0c456fa22c119e
Mustafa Turhan <mustaphaturhan@gmail.com>
2020-09-02 23:08:46 +0300

    fix test problems

--- src/Utils/Queries.js
+++ src/Utils/Queries.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+// eslint-disable-next-line import/prefer-default-export
 export const GET_OAUTH_URL = gql`
   query biliraOAuthUrl($campaignId: String!) {
     biliraOAuthUrl(campaignId: $campaignId) {
